393e3af1f44e13f2ab67adcbd329e3dd887a7682
rohit-iwnl <rohitmanivel9@gmail.com>
2024-03-21 03:15:24 -0700

    initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rohit Manivel on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
